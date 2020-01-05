e3f969ce4cb089019f3d6591ba69836082ff8849
Amren Miller <amrenmiller@Amrens-MacBook-Air.local>
2020-01-05 13:31:29 -0700

    Setup for business-page2

---
+++ assignments/business-page/js/scripts.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+
+    document.querySelector('nav').addEventListener('click', function () {
+        document.querySelector('body').classList.toggle('nav-open')
+    })
+
+}

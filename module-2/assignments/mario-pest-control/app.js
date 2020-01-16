e569f22b81a586c00eba316e502c48079eacd3d0
millerman86 <amrenmiller@gmail.com>
2020-01-16 13:41:03 -0700

    Finish mario icons

--- module-2/assignments/mario-pest-control/app.js
+++ module-2/assignments/mario-pest-control/app.js
@@ -11,3 +11,21 @@ document.getElementById('cheep-cheeps').addEventListener('click', () => {
     document.getElementById('cheep-cheeps-input').stepUp();
 
 })
+
+
+
+
+
+document.getElementById('goomba').nextElementSibling.addEventListener('click', () => {
+    document.getElementById('goomba-input').stepDown();
+})
+
+document.getElementById('bob-ombs').nextElementSibling.addEventListener('click', () => {
+    document.getElementById('bob-ombs-input').stepDown();
+
+})
+
+document.getElementById('cheep-cheeps').nextElementSibling.addEventListener('click', () => {
+    document.getElementById('cheep-cheeps-input').stepDown();
+
+})

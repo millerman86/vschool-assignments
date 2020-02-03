f43954e3a10839daa8326fa2679f546a8994aad0
millerman86 <amrenmiller@gmail.com>
2020-02-03 15:20:08 -0700

    Finish working on pokemon list

--- module-2/assignments/pokemon-list/app.js
+++ module-2/assignments/pokemon-list/app.js
@@ -1,5 +1,17 @@
 function requestListener() {
-    console.log('response', this.responseText);
+    let response = this.responseText;
+    response = JSON.parse(response);
+    
+    let pokemonlist = document.getElementById('pokemonlist');
+
+    
+    response.objects[0].pokemon.forEach((i) => {
+        let newNode = document.createElement('li');
+
+        newNode.innerText = i['name'];
+
+        pokemonlist.appendChild(newNode);
+    });
 }
 
 var request = new XMLHttpRequest();
@@ -8,3 +20,4 @@ request.addEventListener("load", requestListener);
 request.open('GET', 'https://api.vschool.io/pokemon');
 
 request.send();
+

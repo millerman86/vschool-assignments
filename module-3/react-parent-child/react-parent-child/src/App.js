5273d21c1399f3f34c56fa3810cb5a7c5fac1d2a
millerman86 <amrenmiller@gmail.com>
2020-02-07 16:51:09 -0700

    Added react-todo-app

---
+++ module-3/react-parent-child/react-parent-child/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import NavBar from './components/NavBar';
+import MainComponent from './components/MainComponent';
+import Footer from './components/Footer';
+
+
+
+
+
+
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <MainComponent />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;

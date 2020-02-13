bd6dadc35a10286e7fce7e7a0826f3c461475731
millerman86 <amrenmiller@gmail.com>
2020-02-13 09:58:41 -0700

    Create new project rolling dice

--- module-3/dj-react/src/App.js
+++ module-3/dj-react/src/App.js
@@ -1,4 +1,5 @@
-import React, {useEffect} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
 
@@ -8,39 +9,111 @@ function Square(props) {
     <div className="square-container" style={{backgroundColor: props.color}}>
       &nbsp;
     </div>  
-  )
+  );
 }
 
+Square.propTypes = {
+  color: PropTypes.string
+};
+
 function DjButton(props) {
-  return (<button onClick={props.click}>{props.buttonText}</button>)
+  return (<button onClick={() => {
+    props.click();
+
+  }}>{props.buttonText} </button>);
 }
+
+DjButton.propTypes = {
+  click: PropTypes.func, 
+  buttonText: PropTypes.string
+};
 
 
 function SmallTimeDj(props) {
  
   return (
-    <div className="flex dj-component" onClick={props.click}>
+    <div className="flex dj-component" onClick={() =>{
+        props.click();
+        // props.playSound && props.playSound;
+      }}>
       <div className="default white"></div>
       <div className="default black"></div>
       <div className="default black"></div>
       <div classsame="default white"></div>
     </div>
-  )   
+  );
+}
+
+SmallTimeDj.propTypes = {
+  click: PropTypes.func
+};
+
+function BigTimeDj() {
+  return (
+    <div>
+      <div className="flex dj-component">Squares turn into circles</div>
+      <div className="flex dj-component">Circles turn into squares</div>
+      <div className="flex dj-component">All squares turn yellow</div>
+      <div className="flex dj-component">Make 4 sqaures inside each square</div>
+    </div>
+  )
+}
+
+BigTimeDj.propTypes = {
+
 }
 
 function PartyDj(props) {
   return (
-    <div className="flex dj-component" onClick={props.click}>
+    <div className="flex dj-component" onClick={() => {
+        props.click();
+        // playSound();
+      }}>
       <div className="purple"></div>
       <div className="purple"></div>
       <div className=""></div>
       <div className=""></div>
     </div>
-  )
+  );
 }
 
-function ProfessionalDj() {
+PartyDj.propTypes = {
+  click: PropTypes.func
+};
 
+
+function ProfessionalDj(props) {
+  return (
+    <div>
+
+      <div className="flex dj-component" onClick={() => {
+          props.leftClick();
+          // playSound();
+        }}>
+        <div className="black"></div>
+        <div className="black"></div>
+        <div className="blue"></div>
+        <div className="black"></div>
+      </div>
+
+      <div className="flex dj-component" onClick={() => {
+          props.rightClick();
+          // playSound();
+        }}>
+        <div className="black"></div>
+        <div className="black"></div>
+        <div className="black"></div>
+        <div className="blue"></div>
+      </div>
+
+
+    </div>
+  );
+}
+
+ProfessionalDj.propTypes = {
+  leftClick: PropTypes.func,
+  rightClick: PropTypes.func
 }
 
 // function BigTimeDj() {
@@ -73,28 +146,30 @@ class App extends React.Component {
       {
         buttonText: ""
       }, 
-      // {}, 
-      // {}
+
   
       ]
-    }
+    };
 
     this.smallTimeDj = this.smallTimeDj.bind(this);
     this.partyDj = this.partyDj.bind(this);
     this.professionalDj = this.professionalDj.bind(this);
     this.oneOfTheGreats = this.oneOfTheGreats.bind(this);
     this.theGoat = this.theGoat.bind(this);
+
+    this.professionDjLeftButton = this.professionDjLeftButton.bind(this);
+    this.professionalDjRightButton = this.professionalDjRightButton.bind(this);
   }
 
 
   render() {
     const squares = this.state.colors.map((i, index) => {
-      return <Square key={index} color={this.state.colors[index]}/>
-    })
+      return <Square key={index} color={this.state.colors[index]}/>;
+    });
 
     const buttons = this.state.buttons.map((button, index) => {
-      return <DjButton key={index} buttonText={button.buttonText} click={this[button.click]}/>
-    })
+      return <DjButton key={index} blah="blah" buttonText={button.buttonText} click={this[button.click]}/>;
+    });
     
     return (
       <div> 
@@ -104,63 +179,70 @@ class App extends React.Component {
         </div>
 
         {buttons}
+        
         <SmallTimeDj click={this.smallTimeDj} />
         <PartyDj click={this.partyDj} />
-
-        {/* <button onClick={this.smallTimeDj}>Toggle Colors</button>
-        <button onClick={this.partyDj}>Turn Top 2 Squares Purple</button>
-        <button onClick={this.professionalDj}>Turn Bottom 2 Squares Blue: 1 by 1</button> 
-        <button onClick={this.bigTimeDj}>big time dj</button>
-        <button onClick={this.oneOfTheGreats}>one of the greats</button>
-        <button onClick={this.theGoat}>THE G.O.A.T.</button> */}
+        <ProfessionalDj rightClick={this.professionalDjRightButton} leftClick={this.professionDjLeftButton} />
+        <BigTimeDj />
 
       </div>
-    )
+    );
   }
 
   smallTimeDj() {
+    console.log('you found me');
   // You will have one button that will change all four squares either black or white. White if the first one is not white. 
   // Black if the first square is white.
   this.setState((previousState) => {
-    console.log(previousState)
     return {
     
       colors: [...previousState.colors.map((color) => {
         if (color === 'white') {
-          return 'black'
+          return 'black';
         } else if (color === 'black') {
-          return 'white'
+          return 'white';
         } else {
-          return color
+          return color;
         }
       
       })]
-    }
-  })
-
+    };
+  });
   }
-
 
   partyDj() {
   // Add a second button. The second will turn the top half (both squares) of the grid purple.
   this.setState((previousState) => {
     return {
       colors: ['purple', 'purple', previousState.colors[2], previousState.colors[3]]
-    }
-  })
+    };
+  });
 
   }
+
+
 
   professionalDj() {
   // Add two more buttons, for a total of four. These next two will change the colors of the bottom squares blue, 
   // but individually. One will be linked to the bottom left, and the other to the bottom right.
-  
-  
 
+  }
+
+  professionDjLeftButton() {
+    this.setState((previousState) => {
+      return { colors: [previousState.colors[0], previousState.colors[1], 'blue', previousState.colors[3], ] }
+    })
+  }
+
+  professionalDjRightButton() {
+    this.setState((previousState) => {
+      return { colors: [previousState.colors[0], previousState.colors[1], previousState.colors[2], 'blue'] }
+    })
   }
 
   bigTimeDj() {
   // Your next four buttons will each be linked to one of the squares. These buttons can have any affect on those squares that you wish.
+  
 
   }
 
@@ -173,10 +255,29 @@ class App extends React.Component {
   // You will be able to use drop downs/inputs/radio buttons to submit a pattern for your grid to follow over time.
 
   }
-
-
-  
-
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ module-3/rolling-dice/README.md
@@ -0,0 +1,47 @@
+Rolling Dice with React State
+Nate Jensen
+Nate Jensen
+Read more posts by this author.
+
+Nate Jensen
+NATE JENSEN
+19 NOV 2018 • 1 MIN READ
+
+
+
+Using what you have just learned about state in React, you are going to create a <DiceBox /> component that renders 5 dice numbers for the user to view.
+
+You Must
+Create a class component called <DiceBox />
+Write a method in that class that produces 5 random numbers between 1 and 6 and saves them in state.
+Display the 5 random numbers in your <DiceBox /> JSX.
+Provide a button the user can click on that will redo step 2 and present the new numbers to the user.
+Take it further
+Create a <Die /> component that receives a random number as a prop and displays it (rather than just displaying the 5 numbers in the <DiceBox /> JSX).
+Allow the user to click on a given <Die /> to select it, and that number will not change until the dice reset (resetting is done in the next step).
+Have the dice reset (de-selecting all previously selected die and setting their numbers back to 0) after every 3rd roll.
+Hints:
+Consider starting your state off with 5 numbers like this:
+
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            num1: 0,
+            num2: 0,
+            num3: 0,
+            num4: 0,
+            num5: 0
+        }
+    }
+
+With this set up, you then need to write a method that uses this.setState to randomly change the 5 numbers to a number between 1 - 6.
+Ex: Math.floor(Math.random() * 6;
+From there its all about how you want to display those numbers in your return statement.
+
+Resources
+React State
+
+
+Link to this assignment: 
+* https://coursework.vschool.io/dice-react-state/

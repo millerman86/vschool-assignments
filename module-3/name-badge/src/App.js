f62eae8997c0b24c494164d867df4f84f79816bd
millerman86 <amrenmiller@gmail.com>
2020-02-14 10:53:13 -0700

    Finish first step on name-badge app

--- module-3/name-badge/src/App.js
+++ module-3/name-badge/src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      firstName: "", 
+      lastName: "",
+      phoneNumber: "", 
+      placeOfBirth: "", 
+      favoriteFood: "",
+      email: "",
+      bio: ""
+    }
+
+
+    this.handleAllChanges = this.handleAllChanges.bind(this)
+    this.handleFormSubmit = this.handleFormSubmit.bind(this)
+  }
+  
+  
+  handleAllChanges(event) {
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleFormSubmit(e) {
+    e.preventDefault()
+    console.log(this.state)
+  }
+
+  render() {
+
+    return (
+      <div>
+        <form onSubmit={this.handleFormSubmit}>
+          <div>
+            <input name="firstName" placeholder="First Name" value={this.state.firstName} onChange={this.handleAllChanges} />
+            <input name="lastName" placeholder="Last Name" value={this.state.lastName} onChange={this.handleAllChanges} />
+            <input name="email" placeholder="Email" value={this.state.email} onChange={this.handleAllChanges} />
+          </div>
+          <div>
+            <input name="placeOfBirth" placeholder="Place of Birth" value={this.state.placeOfBirth} onChange={this.handleAllChanges} />
+            <input name="phoneNumber" placeholder="Phone" value={this.state.phone} onChange={this.handleAllChanges} />
+            <input name="favoriteFood" placeholder="Favorite Food" value={this.state.favoriteFood} onChange={this.handleAllChanges} />
+          </div>
+          <textarea name="bio" value={this.state.bio} onChange={this.handleAllChanges} placeholder="Tell us about yourself"></textarea>
+          <button>Submit</button>
+        </form>
+
+        {(this.state.firstName || this.state.lastName) ? (<div>{this.state.firstName + " " + this.state.lastName}</div>) : <div>&nbsp;</div>}
+        {this.state.placeOfBirth ? (<div>{this.state.placeOfBirth}</div>) : <div>&nbsp;</div>}
+        {this.state.email ? (<div>{this.state.email}</div>) : <div>&nbsp;</div>}
+        {this.state.phoneNumber ? (<div>{this.state.phoneNumber}</div>) : <div>&nbsp;</div>}
+        {this.state.favoriteFood ? (<div>{this.state.favoriteFood}</div>) : <div>&nbsp;</div>}
+
+      </div>
+    );
+  }
 }
 
 export default App;

b9a604c286f7c59d2c3fb913b931e3b3be3d77e3
millerman86 <amrenmiller@gmail.com>
2020-02-17 13:15:11 -0700

    Set up color generator

---
+++ module-3/random-color-component/README.md
@@ -0,0 +1,1 @@
+Create a component that uses axios to GET a random color from the random color API and sets that component's background color to that random color.
--- module-3/random-color-component/src/App.js
+++ module-3/random-color-component/src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+class App extends React.Component {
+  
+  componentDidMount() {
+    document.querySelector('.color-api').style.background = 'blue'
+
+
+    // You will need to dig out a color from a json object
+    // axios.get('http://www.colr.org/color/random').then(response => {
+    //   console.log(response);
+    // })
+  }
+
+  // componentDidMount() {
+  //   axios.get('https://raw.githubusercontent.com/VSchool/vschool-api/master/static/hitlist.json').then(response => {
+  //     console.log(response);
+  //     this.setState({
+  //       targets: response.data
+  //     })
+  //   })
+  // }
+
+  render() {
+    return (
+      <div className="color-api">
+        sd
+      </div>
+    );
+  }
+  
 }
 
 export default App;

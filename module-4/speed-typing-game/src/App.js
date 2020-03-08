77e8fc56a300e6acef0a0b0c3e210d9dbdc5da47
millerman86 <amrenmiller@gmail.com>
2020-03-08 16:44:03 -0600

    Regular commit, did some work on ugly things

--- module-4/speed-typing-game/src/App.js
+++ module-4/speed-typing-game/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 
-function App() {
 
+function App() {
 
   return (
     <div>
--- module-4/ugly-things/src/App.js
+++ module-4/ugly-things/src/App.js
@@ -5,48 +5,32 @@ import { v4 as uuidv4 } from 'uuid';
 
 
 
-
 function UglyThing(props) {
-  let deleteCircle;
-  if (props.deleteCircle) deleteCircle = 'delete-circle'
-
+ 
   return (
     <div key={props.item['uuid']}>
-    <div>{props.item.title}</div>
-    <div>{props.item.description}</div>
+    <div>{props.item['title']}</div>
+    <div>{props.item['description']}</div>
     
     <div className="image-wrapper"> 
 
+    <img src={props.item.url} alt="" className="ugly-thing-image"/>
 
     <div className="delete-box-aspect-ratio">
-      
-
       <div className="delete-child" onClick={() => {
-
         props.removeUglyThing(props.item['uuid'])
-      }} >
-        
+      }}>
         <div className="inner-two container test">
           <div className="inner-two red">
-        
           </div>
         </div>
-
       </div>
-
     </div>
-
-
-
-
 
       {/* <div onClick={() => {
 
           props.removeUglyThing(props.item['uuid'])
-        }} className={deleteCircle ? deleteCircle : ""}><div className="red-box"></div></div> */}
-
-      <img src={props.item.url} alt="" className="ugly-thing-image"/>
-
+        }} className={props.deleteCircle}><div className="red-box"></div></div> */}
 
       <input name="describetheugly" placeholder="Why is it ugly?" />
     </div>
@@ -90,8 +74,8 @@ class App extends React.Component {
         {context => {
           const RenderedPosts = context.uglyThings.map((item, index) => {
             let deleteCircle;
-            if (this.state.deleteActivated) deleteCircle = true
-            if (!this.state.deleteActivated) deleteCircle = false
+            if (!this.state.deleteActivated) deleteCircle = ""
+            if (this.state.deleteActivated) deleteCircle = "delete-circle"
             
             
             return (
@@ -100,9 +84,11 @@ class App extends React.Component {
           })
           return (
             <div className="page-background">
-              <form onSubmit={(e) => {
-                  this.handleSubmit(e, context)
-                }} name="new-ugly">
+              <form 
+                  name="new-ugly"
+                  onSubmit={(e) => {
+                    this.handleSubmit(e, context)
+                  }}>
 
                 <input name="url" placeholder="Image Url" />
                 <br />

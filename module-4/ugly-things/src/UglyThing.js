1b35cbba9f22ee60e8348680c8f16eecf9f45c1a
millerman86 <amrenmiller@gmail.com>
2020-04-19 18:28:35 -0600

    added old uglythings code

--- module-4/ugly-things/src/UglyThing.js
+++ module-4/ugly-things/src/UglyThing.js
@@ -2,47 +2,45 @@ import React from "react";
 
 export default function UglyThing(props) {
   return (
-    <div key={props.item["uuid"]}>
+    <div
+      key={props.item["uuid"]}
+      onClick={() => {
+        props.pushUUIDFunction(props.item.uuid);
+      }}
+    >
       <div>{props.item["title"]}</div>
       <div>{props.item["description"]}</div>
 
       <div className="image-wrapper">
         <img src={props.item.url} alt="" className="ugly-thing-image" />
 
-        <div
-          className="delete-box-container"
-          style={{
-            border: props.itemsToDelete.includes(props.item.uuid)
-              ? "1px solid white"
-              : "1px solid white",
-          }}
-          onClick={() => {
-            props.pushUUIDFunction(props.item.uuid);
-          }}
-        >
-          <svg
-            viewBox="0 0 24 24"
-            role="presentation"
-            style={{
-              display: props.itemsToDelete.includes(props.item.uuid)
-                ? "block"
-                : "block",
-            }}
-          >
-            <g>
-              <circle
-                r="12"
-                cx="50%"
-                cy="50%"
-                class="circle-back"
-                preserveAspectRatio="xMinYMin meet"
-              />
-            </g>
-            <path
-              color="currentColor"
-              d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"
-            ></path>
-          </svg>
+        <div className="delete-box-container">
+          <div className="delete-child">
+            <div
+              className="svg-container"
+              style={{
+                border: props.itemsToDelete.includes(props.item.uuid)
+                  ? "none"
+                  : "1px solid white",
+              }}
+            >
+              <svg
+                focusable="false"
+                viewBox="0 0 24 24"
+                role="presentation"
+                style={{
+                  display: props.itemsToDelete.includes(props.item.uuid)
+                    ? "block"
+                    : "none",
+                }}
+              >
+                <path
+                  color="currentColor"
+                  d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"
+                ></path>
+              </svg>
+            </div>
+          </div>
         </div>
 
         <input name="describetheugly" placeholder="Why is it ugly?" />

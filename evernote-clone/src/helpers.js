95a78beace592052a50727582d8e250d68ff8e4d
millerman86 <amrenmiller@gmail.com>
2020-04-30 00:02:00 -0600

    add prettier

--- evernote-clone/src/helpers.js
+++ evernote-clone/src/helpers.js
@@ -1,17 +1,41 @@
 // Wait for the user to stop typing for 2 seconds
-export default function debounce(a,b,c){
-    var d,e;
-    return function(){
-      function h(){
-        d=null;
-        c||(e=a.apply(f,g));
-      }
-      var f=this,g=arguments;
-      return (clearTimeout(d),d=setTimeout(h,b),c&&!d&&(e=a.apply(f,g)),e)
+export default function debounce(a, b, c) {
+  var d, e;
+  return function () {
+    function h() {
+      d = null;
+      c || (e = a.apply(f, g));
     }
-  }
-  
-  // for preview on left-hand sidebar
-  export function removeHTMLTags (str) {
-    return str.replace(/<[^>]*>?/gm, '');
+    var f = this,
+      g = arguments;
+    return (
+      clearTimeout(d), (d = setTimeout(h, b)), c && !d && (e = a.apply(f, g)), e
+    );
   };
+}
+
+// for preview on left-hand sidebar
+export function removeHTMLTags(str) {
+  return str.replace(/<[^>]*>?/gm, "");
+}
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+     }
+   

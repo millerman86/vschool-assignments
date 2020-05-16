8ffd11807ea1f92e25cb60ca5b3550dfe4113b40
millerman86 <amrenmiller@gmail.com>
2020-05-16 05:49:08 -0600

    Finished work on auth component

--- module-6/assignments/rock-the-vote/client/src/App.js
+++ module-6/assignments/rock-the-vote/client/src/App.js
@@ -8,40 +8,19 @@ import {
 } from "react-router-dom";
 import Home from "./pages/Home";
 import authService from "./services/authService";
-import Login from "./components/Login";
+import Auth from "./components/Auth";
 import PoliticalIssuesPage from "./pages/PoliticalIssuesPage";
+import { UserContext } from "./context/UserProvider";
 
-const PrivateRoute = ({ component: Component, authorized, ...rest }) => {
-  return (
-    <Route
-      {...rest}
-      render={(props) =>
-        authorized === true ? (
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{ pathname: "/login", state: { from: props.location } }}
-          />
-        )
-      }
-    />
-  );
-};
 
 function App() {
+  const { token, logout } = useContext(UserContext);
   return (
     <Router>
-      <h1>Header</h1> 
+      {token ? (<h1>Header</h1>) : null}
       <Switch>
-        
-        <Route path="/login" component={Login} />
-        <Route path="/" component={Login} />
-        <PrivateRoute
-          exact
-          path="/politicalissues"
-          authorized={loggedIn}
-          component={PoliticalIssuesPage}
-        />
+        <Route exact path="/" component={Login} />
+        <Route path="/auth" component={Auth} />
       </Switch>
     </Router>
   );
---
+++ module-6/assignments/rock-the-vote/client/src/context/UserProvider.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+export const UserContext = React.createContext()
+
+export default function UserProvider(props) {
+    const initState = { user: {}, token: "", issues: []}
+
+    const [userState, setUserState] = useState(initState)
+
+    function signup(credentials) {
+        axios
+            .post('/auth/signup', credentials)
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err))
+    }
+
+    function login(credentials) {
+        axios
+            .post('/auth/login', credentials)
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err))
+    }
+
+    return (
+        <UserContext.Provider
+            value={{
+                ...userState, 
+                signup, 
+                login
+            }}
+        >
+            {props.children}
+        </UserContext.Provider>
+    )
+}

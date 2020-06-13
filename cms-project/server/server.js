bf1674e193c874cb4e98053a7bc3d05bcaf2f7ea
millerman86 <amrenmiller@gmail.com>
2020-06-12 21:42:32 -0600

    Remove files from tracking

--- cms-project/server/server.js
+++
@@ -1,9 +0,0 @@
-let express = require('./config/express');
-
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-let app = express();
-
-app.listen(3001);
-console.log('Server running at http://localhost:3001/');
-module.exports = app;
--- cms-project/src/blitzendpointurl.js
+++
@@ -1,2 +0,0 @@
-export default 'https://api.graph.cool/simple/v1/cj7s1ttoo03v00143o5ylh9od';
-
